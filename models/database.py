17a3f57762368e48ec71b246d7aba66b4c61ef54
Migueldev05 <miguel250515@gmail.com>
2026-02-23 16:35:29 -0300

    16:35

--- models/database.py
+++ models/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import traceback
 import os
 
-load_dotenv() # Procura um arquivo .env com variáveis
+load_dotenv()
 DB_PATH = os.getenv('DATABASE', './data/tarefas.sqlite3')
 
 def init_db(db_name: str = DB_PATH) -> None:
@@ -14,14 +14,12 @@ def init_db(db_name: str = DB_PATH) -> None:
         CREATE TABLE IF NOT EXISTS tarefas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo_tarefa TEXT NOT NULL,
-            data_conclusao TEXT  
+            data_conclusao TEXT,
+            concluida INTEGER DEFAULT 0
         );
         """)
 
 class Database:
-    """
-        Classe que gerencia conexões e operações com um banco de dados SQLite. Utiliza o protocolo de gerenciamento de contexto para garantir que a conexão seja encerrada corretamente.
-    """
     def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
@@ -38,12 +36,9 @@ class Database:
     def close(self) -> None:
         self.connection.close()
 
-    # Métodos para o gerenciamento de contexto
-    # Método de entrada no contexto
     def __enter__(self) -> Self:
         return self
     
-    # Método de saída do contexto
     def __exit__(
             self, 
             exc_type: Optional[Type[BaseException]], 
